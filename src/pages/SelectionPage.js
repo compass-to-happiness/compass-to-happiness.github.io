e62b6fda05d188d6b7798441e1c47d32995345b8
Hiruna Jayamanne <hiru@hiru.dev>
2022-07-09 21:55:57 +1200

    Act upon more pedantic suggestions made by pedantic people

--- src/pages/SelectionPage.js
+++ src/pages/SelectionPage.js
@@ -57,13 +57,13 @@ export default function SelectionPage({ changeView, setSelectedLocation }) {
       </div>
       <img
         src={greenBlob}
-        className={'fixed -z-10'}
+        className="fixed -z-10"
         style={{ top: '-150px', right: '-150px', transform: 'rotate(150deg)' }}
         alt="Green Blob"
       />
       <img
         src={pinkBlob}
-        className={'fixed -z-10'}
+        className="fixed -z-10"
         style={{ bottom: '-250px', left: '-150px', transform: 'rotate(168deg)' }}
         alt="Green Blob"
       />
@@ -78,6 +78,6 @@ const selectionButton = ({ name, image, setSelectedLocation }) => (
     onClick={() => setSelectedLocation(name)}
     style={{ backgroundColor: 'white' }}
   >
-    <img src={image} className={'w-28 h-28 object-contain'} alt={name} />
+    <img src={image} className="w-28 h-28 object-contain" alt={name} />
   </button>
 );
