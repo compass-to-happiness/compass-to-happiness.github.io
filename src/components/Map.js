47ddad1f831fb57d67a02b4220777b733cde965f
Matt Moran <mattmoran2000@gmail.com>
2022-07-10 02:00:24 +1200

    Cache Google API requests for a while.
    - For 'openNow' requests, cache until the next xx:00 or xx:30 time.
    - For requests that may be closed, cache for a day.

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,82 @@
 import { useRef, useEffect, useState } from 'react';
 
+const MINUTE = 60 * 1000;
+const DAY = 24 * 60 * 1000;
+
+function getFromCache(key) {
+  const cached = localStorage.getItem(key);
+  if (cached == null) {
+    return null;
+  }
+  const { expiry, value } = JSON.parse(cached);
+  if (expiry < Date.now()) {
+    localStorage.removeItem(key);
+    return null;
+  }
+  return value;
+}
+
+function setInCache(key, value, expiry) {
+  localStorage.setItem(key, JSON.stringify({ expiry, value }));
+}
+
+function requestToKey(request) {
+  const {
+    keyword,
+    location: { lat, lng },
+    openNow,
+    rankBy,
+    type,
+  } = request;
+  return [keyword, lat.toFixed(4), lng.toFixed(4), openNow, rankBy, type].join('|');
+}
+
+/**
+ * Cache the request result for a while.
+ * For 'openNow' requests, cache until the next xx:00 or xx:30 time.
+ * For requests that may be closed, cache for a day.
+ */
+function addRequestToCache(request, value) {
+  const key = requestToKey(request);
+
+  const now = Date.now();
+  const expiry = request.openNow ? now - now % (30 * MINUTE) + 30 * MINUTE : now - now % DAY + DAY;
+
+  setInCache(key, value, expiry);
+}
+
+function nearbySearchWithCache(mapObj, request, cb) {
+  const cached = getFromCache(requestToKey(request));
+  if (cached != null) {
+    cb(cached);
+    return;
+  }
+
+  const placesService = new window.google.maps.places.PlacesService(mapObj);
+  placesService.nearbySearch(request, (results, status) => {
+    if (status !== 'OK' || results.length === 0) {
+      cb(null)
+      addRequestToCache(request, null);
+      return;
+    }
+  
+    const {
+      name,
+      rating,
+      user_ratings_total,
+      geometry: { location },
+    } = results[0];
+    const result = {
+      name,
+      rating: user_ratings_total === 0 ? null : rating,
+      lat: location.lat(),
+      lng: location.lng(),
+    };
+    cb(result)
+    addRequestToCache(request, result);
+  });
+}
+
 export default function Map({
   center,
   zoom,
@@ -44,24 +121,8 @@ export default function Map({
       type: placeType,
     };
 
-    const placesService = new window.google.maps.places.PlacesService(mapObj);
-    placesService.nearbySearch(request, (results, status) => {
-      if (status === 'OK' && results.length > 0) {
-        const {
-          name,
-          rating,
-          user_ratings_total,
-          geometry: { location },
-        } = results[0];
-        setNearestLocation({
-          name,
-          rating: user_ratings_total === 0 ? null : rating,
-          lat: location.lat(),
-          lng: location.lng(),
-        });
-      }
-    });
-  }, [mapObj, setNearestLocation, keyword, currentLocation, openNow, rankBy, placeType]);
+    nearbySearchWithCache(mapObj, request, result => setNearestLocation(result));
+  }, [mapObj, keyword, currentLocation, openNow, rankBy, placeType, setNearestLocation]);
 
   return <div ref={ref} id="map" className="w-full h-full" />;
 }
