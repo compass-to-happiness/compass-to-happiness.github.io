979e7208376141b65d64ee51e339d122849e858a
ff-dev-45 <flynn.fromont@gmail.com>
2022-07-09 18:42:12 +1200

    Map finally working :)

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,3 +1,19 @@
-export default function Map() {
-  return <div>Error</div>;
+import { useRef, useEffect } from 'react';
+
+const containerStyle = {
+  width: '400px',
+  height: '400px',
+};
+
+export default function Map({ center, zoom }) {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    new window.google.maps.Map(ref.current, {
+      center,
+      zoom,
+      disableDefaultUI: true,
+    });
+  });
+  return <div ref={ref} id="map" style={containerStyle} />;
 }
