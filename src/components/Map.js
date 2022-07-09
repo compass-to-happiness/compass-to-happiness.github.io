f07e2543f2e72e56539a912a4c9c9cd3edb6f828
Matt Moran <mattmoran2000@gmail.com>
2022-07-10 10:45:42 +1200

    Adds location loading from Google Maps (#16)
    * Got location fetch working
    
    * display change for compass and map working
    
    * Fix using string instead of boolean :facepalm:
    
    * Show/hide map, always render
    
    Fix map was hidden instead of visible
    
    * Don't crash when google maps hasn't loaded yet
    
    * setNearestLocation takes `{ name, lat, lng }`
    
    * Update places service to search nearby
    
    * Hack to prevent state reloading when updating the location
    
    * Cache Google API requests for a while.
    
    - For 'openNow' requests, cache until the next xx:00 or xx:30 time.
    - For requests that may be closed, cache for a day.
    
    * Move wrapper higher up the component tree so it loads earlier
    
    * Prettier
    
    * Use device location and selected keyword
    
    * Only mount geo and compass provider once
    
    * Prettier
    
    Co-authored-by: ff-dev-45 <flynn.fromont@gmail.com>

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,19 +1,128 @@
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 
-const containerStyle = {
-  width: '100%',
-  height: '100%',
-};
+const MINUTE = 60 * 1000;
+const DAY = 24 * 60 * 1000;
 
-export default function Map({ center, zoom }) {
+function getFromCache(key) {
+  const cached = localStorage.getItem(key);
+  if (cached == null) {
+    return null;
+  }
+  const { expiry, value } = JSON.parse(cached);
+  if (expiry < Date.now()) {
+    localStorage.removeItem(key);
+    return null;
+  }
+  return value;
+}
+
+function setInCache(key, value, expiry) {
+  localStorage.setItem(key, JSON.stringify({ expiry, value }));
+}
+
+function requestToKey(request) {
+  const {
+    keyword,
+    location: { lat, lng },
+    openNow,
+    rankBy,
+    type,
+  } = request;
+  return [keyword, lat.toFixed(4), lng.toFixed(4), openNow, rankBy, type].join('|');
+}
+
+/**
+ * Cache the request result for a while.
+ * For 'openNow' requests, cache until the next xx:00 or xx:30 time.
+ * For requests that may be closed, cache for a day.
+ */
+function addRequestToCache(request, value) {
+  const key = requestToKey(request);
+
+  const now = Date.now();
+  const expiry = request.openNow ? now - (now % (30 * MINUTE)) + 30 * MINUTE : now - (now % DAY) + DAY;
+
+  setInCache(key, value, expiry);
+}
+
+function nearbySearchWithCache(mapObj, request, cb) {
+  const cached = getFromCache(requestToKey(request));
+  if (cached != null) {
+    cb(cached);
+    return;
+  }
+
+  const placesService = new window.google.maps.places.PlacesService(mapObj);
+  placesService.nearbySearch(request, (results, status) => {
+    if (status !== 'OK' || results.length === 0) {
+      cb(null);
+      addRequestToCache(request, null);
+      return;
+    }
+
+    const {
+      name,
+      rating,
+      user_ratings_total,
+      geometry: { location },
+    } = results[0];
+    const result = {
+      name,
+      rating: user_ratings_total === 0 ? null : rating,
+      lat: location.lat(),
+      lng: location.lng(),
+    };
+    cb(result);
+    addRequestToCache(request, result);
+  });
+}
+
+export default function Map({
+  center,
+  zoom,
+  setNearestLocation,
+  keyword,
+  currentLocation,
+  openNow = true,
+  rankBy = 'distance',
+  placeType = 'food',
+}) {
   const ref = useRef(null);
+  const [mapObj, setMapObj] = useState(null);
 
   useEffect(() => {
-    new window.google.maps.Map(ref.current, {
+    if (window.google?.maps?.Map == null) {
+      console.log('Waiting for Google Maps API to load.');
+      return;
+    }
+    if (ref.current == null) {
+      console.log('ref.current is null');
+      return;
+    }
+    const map = new window.google.maps.Map(ref.current, {
       center,
       zoom,
       disableDefaultUI: true,
     });
+    setMapObj(map);
   }, [center, zoom, ref]);
-  return <div ref={ref} id="map" style={containerStyle} />;
+
+  useEffect(() => {
+    if (mapObj == null) {
+      console.log('mapObj is null');
+      return;
+    }
+
+    const request = {
+      keyword,
+      location: currentLocation,
+      openNow,
+      rankBy: rankBy === 'distance' ? window.google.maps.places.RankBy.DISTANCE : rankBy,
+      type: placeType,
+    };
+
+    nearbySearchWithCache(mapObj, request, (result) => setNearestLocation(result));
+  }, [mapObj, keyword, currentLocation, openNow, rankBy, placeType, setNearestLocation]);
+
+  return <div ref={ref} id="map" className="w-full h-full" />;
 }
--- src/pages/CompassPage.js
+++ src/pages/CompassPage.js
@@ -1,20 +1,26 @@
-import { Wrapper, Status } from '@googlemaps/react-wrapper';
-import Spinner from '../components/Spinner';
-import Error from '../components/Error';
 import Compass from '../components/Compass';
 import Map from '../components/Map';
 import backArrow from '../images/back-arrow.png';
 import { useState } from 'react';
 import { ReactComponent as PinkBlob } from '../images/pink-blob-rotated.svg';
 import MapToggleButton from '../components/MapToggleButton';
+import { useGeo } from '../context/GeoContext';
 
-const render = (status) => {
-  if (status === Status.FAILURE) return <Error />;
-  return <Spinner />;
-};
+export default function CompassPage({ changeView, selectedKeyword }) {
+  const [isMap, setIsMap] = useState(false);
+  const [nearestLocation, setNearestLocation_] = useState(null);
+  const { lat, lng, errorMessage } = useGeo();
 
-export default function CompassPage({ changeView }) {
-  const [isMap, setIsMap] = useState('false');
+  if (errorMessage) {
+    return <div>{errorMessage}</div>;
+  }
+
+  const setNearestLocation = (location) => {
+    if (JSON.stringify(location) !== JSON.stringify(nearestLocation)) {
+      setNearestLocation_(location);
+      console.log('setNearestLocation:', location);
+    }
+  };
 
   return (
     <>
@@ -28,34 +34,37 @@ export default function CompassPage({ changeView }) {
           height: '40%',
         }}
       />
-      {isMap && (
-        <div className="fixed w-full h-3/4 -z-10">
-          <Map center={{ lat: -36.842, lng: 174.757 }} zoom={15} />
-        </div>
-      )}
+      <div className="fixed w-full h-3/4 -z-10" style={{ visibility: isMap ? 'visible' : 'hidden' }}>
+        <Map
+          center={{ lat, lng }}
+          zoom={15}
+          setNearestLocation={setNearestLocation}
+          keyword={selectedKeyword}
+          currentLocation={{ lat, lng }}
+          openNow={true}
+        />
+      </div>
       <div className="h-full">
-        <Wrapper apiKey={process.env.REACT_APP_API_KEY} render={(status) => render(status)}>
-          <div className="w-full p-5 flex flex-col items-center gap-10 h-full">
-            <div>
-              <h3 className="font-bold text-base text-center mb-6 font-s">Compass to Happiness</h3>
-              <button
-                onClick={() => changeView('select')}
-                className="rounded-2xl pt-3 pb-3 px-6 text-base font-semibold bg-primary-200"
-              >
-                <img src={backArrow} alt="Back arrow" className="w-4 inline mr-3"></img>
-                Something Different
-              </button>
-            </div>
-            <div className="h-1/2">
-              <Compass angle="50" className="h-44 w-44" style={{ visibility: isMap ? 'hidden' : '' }} />
-            </div>
-            <div>
-              <h2 className="font-bold text-5xl text-center mb-4">246m Away</h2>
-              <h3 className="font-bold text-base text-center">Gelatiamo</h3>
-              <MapToggleButton onClick={() => setIsMap(!isMap)} isCurrentlyMap={isMap} />
-            </div>
+        <div className="w-full p-5 flex flex-col items-center gap-10 h-full">
+          <div>
+            <h3 className="font-bold text-base text-center mb-6 font-s">Compass to Happiness</h3>
+            <button
+              onClick={() => changeView('select')}
+              className="rounded-2xl pt-3 pb-3 px-6 text-base font-semibold bg-primary-200"
+            >
+              <img src={backArrow} alt="Back arrow" className="w-4 inline mr-3"></img>
+              Something Different
+            </button>
           </div>
-        </Wrapper>
+          <div className="h-1/2">
+            <Compass angle="50" className="h-44 w-44" style={{ visibility: isMap ? 'hidden' : '' }} />
+          </div>
+          <div>
+            <h2 className="font-bold text-5xl text-center mb-4">246m Away</h2>
+            <h3 className="font-bold text-base text-center">Gelatiamo</h3>
+            <MapToggleButton onClick={() => setIsMap(!isMap)} isCurrentlyMap={isMap} />
+          </div>
+        </div>
       </div>
     </>
   );
