ff7c9712bf86f3e008e79ddc6c774f75f916c375
Hiruna Jayamanne <hirunav2@gmail.com>
2022-07-09 17:15:08 +1200

    Add selection page (#8)
    * Add selection page
    
    * Move selection images to the images folder
    
    Co-authored-by: Beverley Sun <beverleysun0205@gmail.com>

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import SelectionPage from './pages/SelectionPage';
 
 function App() {
   const [currentComponent, changeView] = useState('select');
+  const [selectedLocation, setSelectedLocation] = useState(null);
 
   function selectComponent() {
     switch (currentComponent) {
       case 'select':
-        return <SelectionPage changeView={changeView} />;
+        return <SelectionPage changeView={changeView} setSelectedLocation={setSelectedLocation} />;
       case 'compass':
-        return <CompassPage changeView={changeView} />;
+        return <CompassPage changeView={changeView} selectedLocation={selectedLocation} />;
 
       default:
         return <div>No component found</div>;
