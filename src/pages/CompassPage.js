dcf5b2b11e80a62e6f1252f8541fe9a3779839d4
ff-dev-45 <flynn.fromont@gmail.com>
2022-07-09 15:32:35 +1200

    start formatting of compass page

--- src/pages/CompassPage.js
+++ src/pages/CompassPage.js
@@ -1,20 +1,26 @@
 import { Wrapper, Status } from '@googlemaps/react-wrapper';
 import Spinner from '../components/Spinner';
-import ErrorComponent from '../components/ErrorComponent';
+import Error from '../components/Error';
 
-const render = (status) => {
+const render = (status, changeView) => {
   switch (status) {
     case Status.LOADING:
       return <Spinner />;
     case Status.FAILURE:
-      return <ErrorComponent />;
+      return <Error />;
     case Status.SUCCESS:
       return <div> Test ( Compass Component ) </div>;
     default:
-      return <ErrorComponent />;
+      return <Error />;
   }
 };
 
 export default function CompassPage({ changeView }) {
-  return <Wrapper apiKey={process.env.REACT_APP_API_KEY} render={render} />;
+  return (
+    <div className="w-full h-screen p-5 flex flex-col items-center gap-10">
+      <h3>Compass to Happiness</h3>
+      <button className="rounded-2xl border-2 pt-3.5 pb-4 px-10"> &lt;- Something Different</button>
+      <Wrapper apiKey={process.env.REACT_APP_API_KEY} render={(status) => render(status, changeView)} />
+    </div>
+  );
 }
