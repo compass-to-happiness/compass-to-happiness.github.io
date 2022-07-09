00e18c2b5caffdd27f95ff3e817de0baf78e98fd
Matt Moran <mattmoran2000@gmail.com>
2022-07-10 08:48:05 +1200

    Move wrapper higher up the component tree so it loads earlier

--- src/pages/CompassPage.js
+++ src/pages/CompassPage.js
@@ -1,6 +1,3 @@
-import { Wrapper, Status } from '@googlemaps/react-wrapper';
-import Spinner from '../components/Spinner';
-import Error from '../components/Error';
 import Compass from '../components/Compass';
 import Map from '../components/Map';
 import backArrow from '../images/back-arrow.png';
@@ -8,10 +5,6 @@ import { useState } from 'react';
 import { ReactComponent as PinkBlob } from '../images/pink-blob-rotated.svg';
 import MapToggleButton from '../components/MapToggleButton';
 
-const render = (status) => {
-  if (status === Status.FAILURE) return <Error />;
-  return <Spinner />;
-};
 
 export default function CompassPage({ changeView }) {
   const [isMap, setIsMap] = useState(false);
@@ -40,32 +33,32 @@ export default function CompassPage({ changeView }) {
           center={{ lat: -36.842, lng: 174.757 }}
           zoom={15}
           setNearestLocation={setNearestLocation}
-          isHidden={isMap}
+          keyword="ice cream"
+          currentLocation={{ lat: -36.842, lng: 174.757 }}
+          openNow={true}
         />
       </div>
       <div className="h-full">
-        <Wrapper apiKey={process.env.REACT_APP_API_KEY} render={(status) => render(status)} libraries={['places']}>
-          <div className="w-full p-5 flex flex-col items-center gap-10 h-full">
-            <div>
-              <h3 className="font-bold text-base text-center mb-6 font-s">Compass to Happiness</h3>
-              <button
-                onClick={() => changeView('select')}
-                className="rounded-2xl pt-3 pb-3 px-6 text-base font-semibold bg-primary-200"
-              >
-                <img src={backArrow} alt="Back arrow" className="w-4 inline mr-3"></img>
-                Something Different
-              </button>
-            </div>
-            <div className="h-1/2">
-              <Compass angle="50" className="h-44 w-44" style={{ visibility: isMap ? 'hidden' : '' }} />
-            </div>
-            <div>
-              <h2 className="font-bold text-5xl text-center mb-4">246m Away</h2>
-              <h3 className="font-bold text-base text-center">Gelatiamo</h3>
-              <MapToggleButton onClick={() => setIsMap(!isMap)} isCurrentlyMap={isMap} />
-            </div>
+        <div className="w-full p-5 flex flex-col items-center gap-10 h-full">
+          <div>
+            <h3 className="font-bold text-base text-center mb-6 font-s">Compass to Happiness</h3>
+            <button
+              onClick={() => changeView('select')}
+              className="rounded-2xl pt-3 pb-3 px-6 text-base font-semibold bg-primary-200"
+            >
+              <img src={backArrow} alt="Back arrow" className="w-4 inline mr-3"></img>
+              Something Different
+            </button>
           </div>
-        </Wrapper>
+          <div className="h-1/2">
+            <Compass angle="50" className="h-44 w-44" style={{ visibility: isMap ? 'hidden' : '' }} />
+          </div>
+          <div>
+            <h2 className="font-bold text-5xl text-center mb-4">246m Away</h2>
+            <h3 className="font-bold text-base text-center">Gelatiamo</h3>
+            <MapToggleButton onClick={() => setIsMap(!isMap)} isCurrentlyMap={isMap} />
+          </div>
+        </div>
       </div>
     </>
   );
