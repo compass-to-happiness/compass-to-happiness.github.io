a5659d5f45579fc499fcfc33865ba747660a05f1
ff-dev-45 <flynn.fromont@gmail.com>
2022-07-09 15:06:20 +1200

    Setup basic map wrapper and status check

--- src/pages/CompassPage.js
+++ src/pages/CompassPage.js
@@ -1,3 +1,20 @@
+import { Wrapper, Status } from '@googlemaps/react-wrapper';
+import Spinner from '../components/Spinner';
+import ErrorComponent from '../components/ErrorComponent';
+
+const render = (status) => {
+  switch (status) {
+    case Status.LOADING:
+      return <Spinner />;
+    case Status.FAILURE:
+      return <ErrorComponent />;
+    case Status.SUCCESS:
+      return <div> Test ( Compass Component ) </div>;
+    default:
+      return <ErrorComponent />;
+  }
+};
+
 export default function CompassPage({ changeView }) {
-  return <div>Compass Page</div>;
+  return <Wrapper apiKey={process.env.REACT_APP_API_KEY} render={render} />;
 }
