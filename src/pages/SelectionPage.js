10bd268fa624f39639436fb6706c240388f6cf60
Hiruna Jayamanne <hiru@hiru.dev>
2022-07-09 22:41:53 +1200

    Add selection page styling (#11)
    * Add selection page styling
    
    * Act upon suggestions made by pedantic people
    
    * Act upon more pedantic suggestions made by pedantic people
    
    * Fix background blocks to specific size on larger screens
    
    Co-authored-by: Beverley Sun <beverleysun0205@gmail.com>

--- src/pages/SelectionPage.js
+++ src/pages/SelectionPage.js
@@ -6,6 +6,8 @@ import pizzaImage from '../images/pizza.png';
 import friedChickenImage from '../images/fried-chicken.png';
 import alcoholImage from '../images/beer.png';
 import coffeeImage from '../images/coffee.png';
+import greenBlob from '../images/green-blob.svg';
+import pinkBlob from '../images/pink-blob.svg';
 
 export default function SelectionPage({ changeView, setSelectedLocation }) {
   const gotoSelectedLocation = useCallback(
@@ -43,16 +45,28 @@ export default function SelectionPage({ changeView, setSelectedLocation }) {
   ];
 
   return (
-    <div className={'w-full h-screen p-5 flex flex-col items-center gap-10'}>
-      <div className={'text-center flex flex-col gap-10'}>
+    <div className="w-full h-screen p-5 flex flex-col items-center gap-10">
+      <div className="text-center flex flex-col gap-10">
         <h3>Compass to Happiness</h3>
-        <h1 className={'text-5xl'}>What are you feeling?</h1>
+        <h1 className="text-5xl">What are you feeling?</h1>
       </div>
-      <div className={'flex-grow flex items-center'}>
-        <div className={'flex flex-wrap gap-5 justify-center'}>
+      <div className="flex-grow flex items-center">
+        <div className="flex flex-wrap gap-5 justify-center">
           {buttons.map((s) => selectionButton({ ...s, setSelectedLocation: gotoSelectedLocation(s.name) }))}
         </div>
       </div>
+      <img
+        src={greenBlob}
+        className="fixed -z-10 max-w-md"
+        style={{ top: '-150px', right: '-150px', transform: 'rotate(150deg)' }}
+        alt="Green Blob"
+      />
+      <img
+        src={pinkBlob}
+        className="fixed -z-10 max-w-xl"
+        style={{ bottom: '-350px', left: '-200px', transform: 'rotate(168deg)' }}
+        alt="Green Blob"
+      />
     </div>
   );
 }
@@ -60,11 +74,10 @@ export default function SelectionPage({ changeView, setSelectedLocation }) {
 const selectionButton = ({ name, image, setSelectedLocation }) => (
   <button
     key={name}
-    className={
-      'flex button w-40 h-40 radius-lg border-2 drop-shadow-sm drop-shadow-md grid justify-center content-center'
-    }
+    className="grid w-40 h-40 rounded-lg drop-shadow-xl justify-center content-center"
     onClick={() => setSelectedLocation(name)}
+    style={{ backgroundColor: 'white' }}
   >
-    <img src={image} className={'w-28 h-28 object-contain'} alt={name} />
+    <img src={image} className="w-28 h-28 object-contain" alt={name} />
   </button>
 );
