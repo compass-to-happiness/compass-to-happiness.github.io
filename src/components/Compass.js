1f73cf53b29b0fcb624154d17204569627ee0892
Hiruna Jayamanne <hiru@hiru.dev>
2022-07-10 12:11:11 +1200

    Accommodate for the bearing always being negative

--- src/components/Compass.js
+++ src/components/Compass.js
@@ -1,6 +1,9 @@
 import { ReactComponent as CompassSVG } from '../images/compass.svg';
 
 export default function Compass({ angle, style, className }) {
+  const { bearing, errorMessage } = useCompass();
+  const displacement = errorMessage ? 0 : angle + bearing;
+
   const angleStyle = {
     transform: `rotate(${angle}deg)`,
     transition: '0.2s ease-in-out',
